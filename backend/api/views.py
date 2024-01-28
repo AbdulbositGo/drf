148bf07c5cb65ebe0c6b65d18e00398f01720cec
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-01-28 23:40:22 +0500

    model data to json

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import json
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    data = {}
-    try:
-        data = json.loads(request.body)
-    except:
-        pass
-    data['headers'] = dict(request.headers)
-    data['params'] = dict(request.GET)
-    data['content_type'] = request.content_type
+    model_data = Product.objects.all().order_by("?")
+    products = {}
+    if model_data:
+        for i in model_data:
+            products[f'Product {i.id}'] = {
+                "id": i.id,
+                'title': i.title,
+                'content': i.content,
+                'price': i.price
+            }
+    data = {'products': products}
+    
     return JsonResponse(data)
