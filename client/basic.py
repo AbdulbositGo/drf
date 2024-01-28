c9199453a7b2d1ebcb3b768d1871c0206af3de85
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-01-28 21:08:00 +0500

    run django project

--- client/basic.py
+++ client/basic.py
@@ -1,12 +1,16 @@
 import requests as r
 
 
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 response = r.get(endpoint, json={"query": "Hello"})
+
 # print(response) get status code
 # print(response.text) row text response
 # print(response.json())
 # for i, j in response.json().items():
 #     print(f"{i}: {j}")
 # print(response.status_code) get staus code
+
+print(response.text)
